2d0347197cc78d01a2b4a6bb134f3152ecb47474
SeShepherd <misarah.shepherd@gmail.com>
2015-02-03 17:10:34 -0500

    Revert "90% this A2 is complete"
    This reverts commit 8b7da3b0d059717f62c082c5e7627190517387ac.

--- Visibility/public_html/ghostHunt.js
+++ Visibility/public_html/ghostHunt.js
@@ -1,13 +1,12 @@
 /* 
- * Created by Sarah Shepherd
- * CIS4500 Specials Topic
- * University of Guelph
- * Visibility
- * 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * bark.wav came from freesounds.org User: nfrae
  */
 var world, player, light;
-var width = 10;
-var height = 8;
+var width=10;
+var height=8;
 var worldWidth = 88 * width;
 var worldHeight = 88 * height;
 var tileSize = 88;
@@ -15,7 +14,7 @@ var tileSize = 88;
 var enemNum = 4;
 var enemies = [];
 
-var points = 0, counter = 0, recount = 0;
+var points = 0, counter = 0, recount=0;
 
 window.addEventListener("keydown", keyDownEvent, false);
 
@@ -24,30 +23,29 @@ function init() {
     world = new World(worldWidth, worldHeight, tileSize, 0);
     //create dog
     player = new Sprite(world, tileSize, tileSize, "images/PlayerL.png", 4, 5, 2);
-    //The dog's light
+    // world.setGridVal(4, 5, 2);
     light = new Sprite(world, tileSize, tileSize, "images/light.png", 3, 5, 3);
+    // world.setGridVal(3, 5, 3);
     setLight();
 
-    //Create Ghost
+
     for (var i = 0; i < enemNum; i++) {
-        enemies[i] = new Sprite(world, tileSize, tileSize, "images/ghostHL.png", i * tileSize, worldHeight - (tileSize * i), 1);
+        enemies[i] = new Sprite(world, tileSize, tileSize, "images/ghostHL.png", i*tileSize, worldHeight-(tileSize*i), 1);
         addGhost(i);
 
     }
 
-    //Game Play points and timer
     points = 0;
     document.getElementById("bgNoise").play();
 
     world.start();
+    // timer.start();
 
 }
 function setLight() {
-    //Get XY position of dog
     var x = player.getX();
     var y = player.getY();
 
-    //Control the battery of the flashlight/recharge
     if (light.isVisible()) {
         counter++;
     } else {
@@ -57,10 +55,9 @@ function setLight() {
     if (counter > 3) {
         light.hide();
         counter = 0;
-        recount = 4;
+        recount=4;
     }
-
-    //place light in front of dog
+  
     if (player.getDir() == "Left") {
         light.setImage("images/light.png");
         light.setX((x - tileSize) / tileSize);
@@ -71,17 +68,20 @@ function setLight() {
     light.setY(y / tileSize);
 }
 
+/*Keyboard event handler*/
 function keyDownEvent(e) {
     var oldX, oldY, x = -1, y = -1;
-    //Get player XY
     oldX = player.getX();
     oldY = player.getY();
-    //Determine which key is pressed
+    // console.log("X: "+oldX+"Y: "+oldY);
+    /*Determine which key is pressed*/
     switch (e.keyCode) {
-        //potenial bark or light command
+
+        //poetnial bark or light command
         // Space key pressed
         case 32:
-            if (!light.isVisible() && recount <= 0) {
+            // console.log("here");
+            if (!light.isVisible()&&recount<=0) {
                 light.show();
             } else {
                 light.hide();
@@ -90,7 +90,8 @@ function keyDownEvent(e) {
 
             //Left key
         case 37:
-            e.preventDefault();
+            // e.keyCode;
+            // console.log(player.getDir());
             if (player.getDir() === "Right") {
                 player.setImage("images/PlayerL.png");
                 player.setDir("Left");
@@ -99,11 +100,13 @@ function keyDownEvent(e) {
                 if (checkBordX(x))
                     player.setX(x / tileSize);
             }
+
             break;
 
             //Up key
         case 38:
             e.preventDefault();
+            // e.keyCode;
             y = oldY - (tileSize / 2);
             if (checkBordY(y))
                 player.setY(y / tileSize);
@@ -111,7 +114,7 @@ function keyDownEvent(e) {
 
             //Right Key    
         case 39:
-            e.preventDefault();
+            // e.keyCode;
             if (player.getDir() === "Left") {
                 player.setImage("images/PlayerR.png");
                 player.setDir("Right");
@@ -124,6 +127,7 @@ function keyDownEvent(e) {
 
             //Down Key
         case 40:
+            //e.keyCode;
             e.preventDefault();
             y = oldY + tileSize / 2;
             if (checkBordY(y))
@@ -131,12 +135,14 @@ function keyDownEvent(e) {
             break;
     }
 
-//Move or update these after every key press (valid or not)
     setLight();
     EnemyMoves();
     lightCheck();
     deadDog();
+   
 }
+
+/***Boarder detection ***/
 function checkBordY(newPos) {
     if (newPos >= 0 && newPos < worldHeight) {
         return true;
@@ -154,179 +160,181 @@ function checkBordX(newPos) {
     return false;
 }
 
+
+/*Ghost Enemies functions*/
 function EnemyMoves() {
     var move;
     var playerX = player.getX();
     var playerY = player.getY();
-
+    
     for (var i = 0; i < enemNum; i++) {
         var x, y, oldX, oldY;
-        //Enemies visible only when light is dim
         if (light.isVisible()) {
-            enemies[i].hide();
+           enemies[i].hide();
         } else {
             enemies[i].show();
         }
-        //If player is movable
-        if (enemies[i].getId() != 0) {
-            //pick randomly to move either an x or y position for ghost
-            move = Math.floor(Math.random() * 2);
-            //Get current position
-            oldX = enemies[i].getX();
-            oldY = enemies[i].getY();
+        //console.log(enemies[i].getId());
+        if(enemies[i].getId()!=0){
 
-            //Move x postion or y position of ghost
-            if (move === 0) {
-                if (oldX >= playerX) {
-                    x = (oldX - tileSize / 2) / tileSize;
-                    enemies[i].setDir("Left");
-                    enemies[i].setImage("images/ghostHL.png");
-                } else {
-                    x = (oldX + tileSize / 2) / tileSize;
-                    enemies[i].setDir("Right");
-                    enemies[i].setImage("images/ghostHR.png");
-                }
-                if (checkBordX(x) && checkGhost(i))
-                    enemies[i].setX(x);
+        //pick randomly to move either an x or y position for ghost
+        move = Math.floor(Math.random() * 2);
+        //Get current position
+        oldX = enemies[i].getX();
+        oldY = enemies[i].getY();
+
+        //Move x postion or y position of ghost
+        if (move === 0) {
+            if (oldX >= playerX) {
+                x = (oldX - tileSize / 2) / tileSize;
+                enemies[i].setDir("Left");
+                enemies[i].setImage("images/ghostHL.png");
             } else {
-                if (oldY >= playerY) {
-                    y = (oldY - tileSize / 2) / tileSize;
-                } else {
-                    y = (oldY + tileSize / 2) / tileSize;
-                }
-                if (checkBordY(y) && checkGhost(i))
-                    enemies[i].setY(y);
+                x = (oldX + tileSize / 2) / tileSize;
+                enemies[i].setDir("Right");
+                enemies[i].setImage("images/ghostHR.png");
             }
-
+            if (checkBordX(x)&&checkGhost(i))
+                enemies[i].setX(x);
         } else {
-            //Non-movable ghost has been caught
-            //Restart ghost
-            addGhost(i);
+            if (oldY >= playerY) {
+                y = (oldY - tileSize / 2) / tileSize;
+            } else {
+                y = (oldY + tileSize / 2) / tileSize;
+            }
+            if (checkBordY(y)&&checkGhost(i))
+                enemies[i].setY(y);
         }
+
+    }else{
+        console.log("replace ghost "+i);
+        addGhost(i);
     }
+}
 
 
 }
 function addGhost(i) {
     var y = Math.floor(Math.random() * height);
     var side = Math.floor(Math.random() * 2);
-    //Set various values of ghost
+    
     enemies[i].setId(1);
     if (side === 1)
     {
-        side = width - 1;
+        side = width-1;
         enemies[i].setDir("Left");
         enemies[i].setImage("images/ghostHL.png");
-    } else {
-        enemies[i].setDir("Right");
+     }else{
+         enemies[i].setDir("Right");
         enemies[i].setImage("images/ghostHR.png");
-    }
-    while (!checkGhost(i)) {
-        //check again
-    }
+     }
+     while(!checkGhost(i)){
+    //check again
+     }
     enemies[i].setX(side);
     enemies[i].setY(y);
-    //console.log(" xy:" + enemies[i].getX() + " " + enemies[i].getY());
+    
+
+    
+    console.log(" xy:" + enemies[i].getX() + " " + enemies[i].getY());
+
 }
-function checkGhost(num) {
-    /*This function doesn't work as well as I want*/
-    var cX, cY, eX, eY;
+function checkGhost(num){
+    var cX,cY,eX,eY;
     cX = enemies[num].getX();
     cY = enemies[num].getY();
-  //ghost should not be on top of each other
+    //console.log("cur "+num+" cX: "+cX+"cY: "+cY);
     for (var i = num; i >= 0; i--) {
-        if (i != num) {
-            eX = enemies[i].getX();
-            eY = enemies[i].getY();
+        
+        if(i!=num){
+            eX=enemies[i].getX();
+            eY=enemies[i].getY();
             //console.log(i+" eX: "+eX+"eY: "+eY);
-            if ((cX > eX && cX < (eX + tileSize) || (cX + tileSize) > eX && (cX + tileSize) < (eX + tileSize)) && (cY > eY && cY < (eY + tileSize) || (cY + tileSize) > eY && (cY + tileSize) < (eY + tileSize))) {
+            if((cX>eX&&cX<(eX+tileSize)||(cX+tileSize)>eX&&(cX+tileSize)<(eX+tileSize))&& (cY>eY&&cY<(eY+tileSize)||(cY+tileSize)>eY&&(cY+tileSize)<(eY+tileSize))){
                 return false;
             }
         }
-
+        
     }
     return true;
-
+    
 }
 
-function lightCheck() {
-    var cX, cY, eX, eY;
+function lightCheck(){
+    var cX,cY,eX,eY;
     cX = light.getX();
     cY = light.getY();
-    //Light should disintigrate a ghost
+    //console.log("light cX: "+cX+" "+(cX+tileSize)+"cY: "+cY+" "+(cY+tileSize));
     for (var i = 0; i < enemNum; i++) {
-        eX = enemies[i].getX();
-        eY = enemies[i].getY();
-        
-        if ((cX >= eX && cX < (eX + tileSize) || (cX + tileSize) >= eX && (cX + tileSize) < (eX + tileSize)) && (cY >= eY && cY < (eY + tileSize) || (cY + tileSize) >= eY && (cY + tileSize) < (eY + tileSize))) {
-            if (light.isVisible()) {
-                //replace the image of the ghost
-                if (player.getDir() == "Left") {
-                    light.setImage("images/ghostUnL2.png");
-                } else {
-                    light.setImage("images/ghostUnR.png");
-                }
-
-                enemies[i].setId(0);
-                gamePoints(1);
+            eX=enemies[i].getX();
+            eY=enemies[i].getY();
+            //console.log(i+" eX: "+eX+" "+(eX+tileSize)+"eY: "+eY+" "+(eY+tileSize));
+             if((cX>=eX&&cX<(eX+tileSize)||(cX+tileSize)>=eX&&(cX+tileSize)<(eX+tileSize))&& (cY>=eY&&cY<(eY+tileSize)||(cY+tileSize)>=eY&&(cY+tileSize)<(eY+tileSize))){
+                 if(light.isVisible()){
+                     //enemies[i].show();
+                     if(player.getDir()=="Left"){
+                         light.setImage("images/ghostUnL2.png");
+                     }else{
+                         light.setImage("images/ghostUnR.png");
+                     }
+                     
+                     enemies[i].setId(0);
+                     gamePoints(1);
+                 }
             }
-        }
+       
     }
+    //console.log("..");
+    
+    
 }
-function deadDog() {
-    var cX, cY, eX, eY;
+function deadDog(){
+    var cX,cY,eX,eY;
     cX = player.getX();
     cY = player.getY();
+    //console.log("light cX: "+cX+" "+(cX+tileSize)+"cY: "+cY+" "+(cY+tileSize));
     for (var i = 0; i < enemNum; i++) {
-        eX = enemies[i].getX();
-        eY = enemies[i].getY();
-        if ((cX >= eX && cX < (eX + tileSize / 2) || (cX + tileSize / 2) >= eX && (cX + tileSize / 2) < (eX + tileSize / 2)) && (cY >= eY && cY < (eY + tileSize / 2) || (cY + tileSize / 2) >= eY && (cY + tileSize / 2) < (eY + tileSize / 2))) {
-            //show Enemy that got the dog
-            enemies[i].show();
-            //GamePlay mechanics
-            gamePoints(0);
-            //restart ghost
-            addGhost(i);
-
-        }
-
+            eX=enemies[i].getX();
+            eY=enemies[i].getY();
+            //console.log(i+" eX: "+eX+" "+(eX+tileSize)+"eY: "+eY+" "+(eY+tileSize));
+             if((cX>=eX&&cX<(eX+tileSize/2)||(cX+tileSize/2)>=eX&&(cX+tileSize/2)<(eX+tileSize/2))&& (cY>=eY&&cY<(eY+tileSize/2)||(cY+tileSize/2)>=eY&&(cY+tileSize/2)<(eY+tileSize/2))){
+                 
+                     enemies[i].show();
+                    
+                     gamePoints(0);
+                 
+            }
+       
     }
+    //console.log("..");
+    
+    
 }
 
 function gamePoints(condition) {
-    //Update score
-    if (condition == 1) {
-        points++;
-        document.getElementById("bark").play();
-    } else {
+    if(condition==1){
+    points++;
+    document.getElementById("bark").play();
+    }else{
         document.getElementById("laugh").play();
-        points--;
+       points--;
     }
-    document.getElementById("points").innerHTML=points;
-    //console.log("Points: " + points);
+console.log("Points: "+points); 
 
 
 }
-function checkEnd() {
-    if (document.getElementById("bgNoise").paused) {
-
-        //Show score and restart or leave page
-        if (window.confirm("Final score was " + points + "!\nLet's play it again?")) {
-            points = 0;
-            init();
-
-        } else {
-
-            //console.log("Game Over");
-            //Disable controls
-            window.removeEventListener("keydown", keyDownEvent, false);
-            world.stop();
-        }
+function checkEnd(){
+    if(document.getElementById("bgNoise").paused){
+     console.log("Game Over");    
     }
 }
 
 
 function update() {
+
+
+
+
     world.clear();
     player.update();
     light.update();
@@ -334,5 +342,5 @@ function update() {
         enemies[i].update();
     }
     checkEnd();
-
+    
 }
