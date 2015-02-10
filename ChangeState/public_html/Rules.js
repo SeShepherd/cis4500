c49898eeaeb98a8c9db407560f252cef31187fb0
SeShepherd <misarah.shepherd@gmail.com>
2015-02-10 08:37:04 -0500

    Change of state (rough version)

--- ChangeState/public_html/Rules.js
+++ ChangeState/public_html/Rules.js
@@ -1,107 +1,320 @@
 /* 
  * Created by Sarah Shepherd
- * CIS*
+ * CIS*4500
  * University of Guelph
  * 
- 
- bounce sound by: gezortenplotz;
- 
- 
- update objects
+ * Change of State
  */
-var player, mirrors = [], win;
-var level = 0, screen;
-var count;
-var mirNum = 15;
-var bullet, bullCount = 0;
+
+var level = 0;
+var screen;
+var player = [], playerNum = 3, current = 0;
+var key = 0, timer = 0, wallNum = 6;
+var wall = [], hook, button, elevator, ladder;
+var eve, condition = [], cNum = 2, goal;
 
 
-window.addEventListener("keydown", keyDownEvent, false);
+
 function State(screen) {
-
-
     this.screen = screen;
+    drawObjects();
+    drawLevel();
 
-    createLevel();
     //Create three players
-    //player = new Sprite(screen, screen.tileWidth * 2, screen.tileWidth * 2, "images/player.png", 0, 0, 1);
+    player[0] = new Sprite(screen, screen.tileWidth, screen.tileWidth * 2, "images/SM.png", 0, 200, 1);
+    player[0].createAnimation(200, 200, 4, 2);
+    player[1] = new Sprite(screen, screen.tileWidth, 75, "images/SF.png", 0, 225, 1);
+    player[1].createAnimation(200, 150, 4, 2);
+    player[2] = new Sprite(screen, screen.tileWidth, screen.tileWidth, "images/SS.png", 0, 250, 1);
+    player[2].createAnimation(200, 100, 4, 2);
+
+    eve = window;
+    eve.addEventListener("keydown", keyDownEvent, false);
+    window.addEventListener("keyup", keyUpEvent, false);
+
 
     screen.start();
 }
 
 function update() {
     var i;
-
     screen.clear();
-   // player.update();
+    drawLevel();
+    // player.animate();
+    for (i = 0; i < playerNum; i++) {
+        player[i].update();
+    }
 
+    checkObjCollisions();
+    endGoal();
+}
+
+function keyUpEvent(e) {
+    key = 0;
 }
 
 function keyDownEvent(e) {
-    var oldY;
-    oldY = player.getY();
+    var x, y;
+    var xPos, yPos;
+    var move;
+
+    x = player[current].getX();
+    y = player[current].getY();
+    xPos = x + player[current].getWidth() / 2;
+    yPos = x + player[current].getHeight();
 
     /*Determine which key is pressed*/
     switch (e.keyCode) {
-
         //poetnial bark or light command
         // Space key pressed
+
         case 32:
             e.preventDefault();
             //Change character
-            
+            current = (current + 1) % 3;
+            break;
+
+            //Left key
+        case 37:
+            e.preventDefault();
+            move = 1;
+            //check walls before moving
+            //rightside and leftside
+            for (var i = 3; i <= wallNum; i++) {
+                if (((x + 10) > wall[i][0]) && ((x + 10) < (wall[i][0] + wall[i][2]))) {
+                    if ((y > wall[i][1]) && (y < (wall[i][1] + wall[i][3]))) {
+                        move = 0;
+                    }
+                }
+            }
+
+            for (var i = 0; i < cNum; i++) {
+                if (((x + 10) >= condition[i][0]) && ((x + 10) < (condition[i][0] + condition[i][2]))) {
+                    if ((y >= condition[i][1]) && (y < (condition[i][1] + condition[i][3]))) {
+                        move = 0;
+                    }
+                }
+            }
+
+            if (move === 1) {
+                player[current].aniDir(1);
+                player[current].setX(x - 5);
+            }
             break;
 
             //Up key
         case 38:
             e.preventDefault();
-          
+
+            //if player 0/1 AND collision with ladder
+            //increase x and update until ladder done;
+            if (current !== 2) {
+
+                if ((xPos >= ladder.getX()) && (xPos <= (ladder.getX() + ladder.getWidth()))) {
+                    if (((y + player[current].getHeight()) >= (ladder.getY() + 20)) && ((y + player[current].getHeight()) <= (ladder.getY() + ladder.getHeight()))) {
+                        i = 0;
+                        eve.removeEventListener("keydown", keyDownEvent, false);
+                        var check = function () {
+                            if (i > 175) {
+                                // run when condition is met move on
+                                eve.addEventListener("keydown", keyDownEvent, false);
+                            }
+                            else {
+                                player[current].setY(y - i);
+                                player[current].animate();
+                                update();
+                                i += 5;
+                                setTimeout(check, 75); // check again in a second
+                            }
+                        };
+                        check();
+                    }
+                }
+            }
+//
             break;
 
-            //Down Key
-        case 40:
-            //e.keyCode;
+            //Right Key    
+        case 39:
             e.preventDefault();
-            if (checkBordY(oldY + 50))
-                player.setY(oldY + 25);
+            //check walls before moving
+            move = 1;
+            for (var i = 3; i <= wallNum; i++) {
+                if ((xPos >= wall[i][0]) && (xPos < (wall[i][0] + wall[i][2]))) {
+                    if ((y >= wall[i][1]) && (y < (wall[i][1] + wall[i][3]))) {
+                        move = 0;
+                    }
+                }
+            }
+            for (var i = 0; i < cNum; i++) {
+                if (((xPos) >= condition[i][0]) && ((xPos) < (condition[i][0] + condition[i][2]))) {
+                    if ((y >= condition[i][1]) && (y < (condition[i][1] + condition[i][3]))) {
+                        move = 0;
+                    }
+                }
+            }
+            if (move === 1) {
+                player[current].aniDir(0);
+                player[current].setX(x + 5);
+            } else {
+                player[current].setX(x - 5);
+            }
             break;
+            //Down Key (NOT AVAILABLE IN THIS GAME
     }
 
+    player[current].animate();
 }
 /***Boarder detection ***/
 function checkBordY(newPos) {
-    if (newPos >= 0 && newPos < (screen.height - (screen.tileWidth / 3))) {
+    if (newPos >= 0 && newPos < screen.height) {
         return true;
     }
-    //Outside of boarders
+//Outside of boarders
     return false;
 }
 function checkBordX(newPos) {
     if (newPos >= 0 && newPos < screen.width) {
         return true;
     }
-    //Outside of boarders
+//Outside of boarders
     return false;
 }
 
-function createLevel() {
-
+function drawLevel() {
     var i, j, k = 0;
-    switch (level) {
-        case 0:
-          
-            level++;
-            break;
-        case 1:
-           
-            break;
+    screen.context.fillStyle = "#7E2D6C";
+    for (i = 0; i <= wallNum; i++) {
+        if (i == 6)
+            screen.context.fillStyle = "#C6D2E7";
+        screen.context.fillRect(wall[i][0], wall[i][1], wall[i][2], wall[i][3]);
+    }
+    for (i = 0; i < cNum; i++) {
+        screen.context.fillStyle = "#C6D2E7";
+        screen.context.fillRect(condition[i][0], condition[i][1], condition[i][2], condition[i][3]);
+    }
 
-        case 2:
-          
-            break;
 
+
+    hook.update();
+    elevator.update();
+    button.update();
+    ladder.update();
+    goal.update();
+}
+
+function drawObjects() {
+    wall[0] = [0, screen.height - 10, screen.width, screen.height];
+    wall[1] = [0, 0, screen.width, 10];
+    wall[2] = [0, 115, screen.width, 10];
+    wall[3] = [0, 0, 10, 200];
+    wall[4] = [460, 115, 10, 120];
+    wall[5] = [screen.width - 10, 115, 10, 190];
+    wall[6] = [340, 10, 10, 105];
+
+    condition[0] = [0, 175, 10, 120];
+    condition[1] = [590, 5, 10, 115];
+
+    hook = new Sprite(screen, screen.tileWidth, screen.tileWidth / 2, "images/hook.png", 440, 10, 2);
+    //button= new Sprite(screen, screen.tileWidth, screen.tileWidth/2, "images/button.png", 150,  95, 2);
+    button = new Sprite(screen, screen.tileWidth, screen.tileWidth / 2, "images/button.png", 150, screen.height - 30, 2);
+    elevator = new Sprite(screen, screen.tileWidth, screen.tileWidth / 2, "images/Lift.png", 380, screen.height - 35, 2);
+    ladder = new Sprite(screen, screen.tileWidth, 185, "images/ladder.png", 240, 115, 2);
+
+    goal = new Sprite(screen, 25, 25, "images/key.png", 550, screen.height - 35, 2);
+}
+
+function checkObjCollisions() {
+//player[1] collides with button
+
+    if (player[1].collision(button.getX(), button.getY(), button.getWidth(), button.getHeight())) {
+        wallNum = 5;
+        // console.log("button press");
+        // MAKE wall[5] Disappear
+    } else {
+        wallNum = 6;
+    }
+
+    var i, j;
+
+
+//player[0] collides with hook
+
+    if (player[0].collision(hook.getX() + 10, hook.getY(), hook.getWidth() - 10, hook.getHeight())) {
+        //console.log(elevator.getY(),"hook press");
+
+
+
+        for (i = 1; i < 3; i++) {
+            if (player[i].collision(elevator.getX(), elevator.getY(), elevator.getWidth(), elevator.getHeight())) {
+                //player[i].setY(player[i].getY()-180);
+                elevator.setY(95);
+                player[i].setY(125 - player[i].getHeight());
+            }
+        }
+        elevator.setY(95);
+
+        //make elavator move <- Maybe not
+
+
+
+    } else {
+        elevator.setY(screen.height - 35);
+    }
+
+//if player colides with key
+    for (i = 0; i < 3; i++) {
+        if (player[i].collision(goal.getX(), goal.getY(), goal.getWidth(), goal.getHeight())) {
+            //remove key
+            ////remove collision
+            goal.hide();
+            cNum = 1;
+
+        }
 
     }
 
 
+}
+
+function endGoal() {
+
+    if (!goal.isVisible()) {
+
+        if (((player[0].getX() + player[0].getWidth()) >= screen.width) && ((player[1].getX() + player[1].getWidth()) > screen.width) && (player[2].getX() + player[2].getWidth()) > screen.width) {
+            //End Game
+            document.getElementById("win").play();
+
+            screen.context.font = "30px Verdana";
+            // Create gradient
+            var gradient = screen.context.createLinearGradient(0, 0, screen.width, 0);
+            gradient.addColorStop("0", "magenta");
+            gradient.addColorStop("0.5", "blue");
+            gradient.addColorStop("1.0", "red");
+            // Fill with gradient
+            screen.context.fillStyle = gradient;
+            screen.context.fillText("Mission Accomplished!", 120, 90);
+
+//stop screen completely
+            screen.stop();
+        }
+    }
+}
+
+function Reset() {
+    drawObjects();
+    drawLevel();
+
+    //Create three players
+    player[0] = new Sprite(screen, screen.tileWidth, screen.tileWidth * 2, "images/SM.png", 0, 200, 1);
+    player[0].createAnimation(200, 200, 4, 2);
+    player[1] = new Sprite(screen, screen.tileWidth, 75, "images/SF.png", 0, 225, 1);
+    player[1].createAnimation(200, 150, 4, 2);
+    player[2] = new Sprite(screen, screen.tileWidth, screen.tileWidth, "images/SS.png", 0, 250, 1);
+    player[2].createAnimation(200, 100, 4, 2);
+
+    eve = window;
+    eve.addEventListener("keydown", keyDownEvent, false);
+    window.addEventListener("keyup", keyUpEvent, false);
+    screen.start();
 }
--- ChangeState/public_html/gameEngine.js
+++ ChangeState/public_html/gameEngine.js
@@ -1,6 +1,6 @@
 /* 
  * Created by Sarah Shepherd
- * CIS4500 Specials Topic
+ * CIS*4500 Specials Topic
  * University of Guelph
  * 
  * GameEngine.js basics
@@ -8,9 +8,10 @@
 
 
 //Canvas will become game world area
-function World(width, height, tileSize,id) {
+function World(width, height, tileSize, id) {
 
-    //Create the canvas
+    //Set the canvas
+    //this.canvas = document.getElementById("canvas");
     this.canvas = document.createElement("canvas");
     document.body.appendChild(this.canvas);
     this.context = this.canvas.getContext("2d");
@@ -20,15 +21,26 @@ function World(width, height, tileSize,id) {
     this.height = height;
     this.canvas.width = this.width;
     this.canvas.height = this.height;
-    
 
-   
+    //Set the world matrix
     this.tileWidth = tileSize;
+    this.id = id;
+    this.grid = new Array(this.width / this.tileWidth);
+    for (i = 0; i < this.width / this.tileWidth; i++) {
+
+        this.grid[i] = new Array(this.height / this.tileWidth);
+        for (j = 0; j < this.height / this.tileWidth; j++)
+            this.grid[i][j] = id;
+
+    }
+    
+    //this.canvas.style.backgroundColor = "cyan";
+
 }
 World.prototype.start = function () {
 
     //Set the method "update" to run every 50 milliseconds
-    this.updateTime = setInterval(update, 100);
+    this.updateTime = setInterval(update, 50);
 };
 World.prototype.clear = function () {
 
@@ -40,9 +52,20 @@ World.prototype.stop = function () {
     clearInterval(this.updateTime);
 
 };
+World.prototype.getGridVal = function (i,j) {
+
+    return this.grid[i][j];
+
+};
+World.prototype.setGridVal = function (i,j,id) {
+
+    this.grid[i][j]=id;
+
+};
 
 
-function Sprite(world, width, height, image, x, y,id) {
+
+function Sprite(world, width, height, image, x, y, id) {
 
     this.world = world;
     this.canvas = world.canvas;
@@ -53,15 +76,16 @@ function Sprite(world, width, height, image, x, y,id) {
 
     this.image = new Image();
     this.image.src = image;
-    this.direction = "Left";
+    this.direction = "right";
 
-    this.x = x*this.width;
-    this.y = y*this.height;
-     
-    this.context.drawImage(this.image,x,y);
-    this.visible=true;
+    this.x = x;
+    this.y = y;
 
-};
+    this.context.drawImage(this.image, x, y);
+    this.visible = true;
+    this.animation=0;
+}
+;
 
 Sprite.prototype.setImage = function (image) {
 
@@ -70,24 +94,24 @@ Sprite.prototype.setImage = function (image) {
 };
 Sprite.prototype.setDir = function (direction) {
 
-    this.direction=direction;
+    this.direction = direction;
 
 };
 Sprite.prototype.setX = function (x) {
 
-    this.x = x*this.width;
+    this.x = x;
     this.xPos = (this.x * this.world.tileWidth) + (this.world.tileWidth / 2);
 
 };
 Sprite.prototype.setY = function (y) {
 
-    this.y = y*this.height;
+    this.y = y;
     this.yPos = (this.y * this.world.tileWidth) + (this.world.tileWidth / 2);
 
 };
 Sprite.prototype.setId = function (id) {
 
-    this.id=id;
+    this.id = id;
 
 };
 Sprite.prototype.getImage = function () {
@@ -98,6 +122,16 @@ Sprite.prototype.getImage = function () {
 Sprite.prototype.getDir = function () {
 
     return this.direction;
+
+};
+Sprite.prototype.getHeight = function () {
+
+    return this.height;
+
+};
+Sprite.prototype.getWidth = function () {
+
+    return this.width;
 
 };
 Sprite.prototype.getX = function () {
@@ -115,23 +149,67 @@ Sprite.prototype.getId = function () {
     return this.id;
 
 };
-Sprite.prototype.transform = function (id, image) {
+//Sprite.prototype.changeImg = function (id, image) {
 
-    
-    this.setImage(image);
+
+  //  this.setImage(image);
 
     //this.world.setGridVal(this.x, this.y, this.idNum);
 
+//};
+
+Sprite.prototype.createAnimation=function(frameWidth,frameHeight,numFrames,rows){
+    	
+
+	this.imageWidth = frameWidth;
+	this.imageHeight = frameHeight;
+		
+    this.frameIndex=0;
+    this.rowIndex=0;
+    this.tickCount=0;
+    this.numFrames=numFrames; //4
+    this.rows=rows; //2
+    this.animation=1;
+    
+    //???????
+  
+
 };
+Sprite.prototype.animate = function(){
+       this.tickCount++;
+
+            if (this.tickCount > 3) {
+
+				this.tickCount = 0;
+				
+                // If the current frame index is in range
+                if (this.frameIndex < this.numFrames-1) {	
+                    // Go to the next frame
+                    this.frameIndex += 1;
+                } else {
+                    this.frameIndex = 0;
+                }
+            }
+     //       this.context.drawImage(this.image,this.frameIndex*this.imageWidth/this.numFrames,this.rowIndex*this.imageHeight/this.rows,this.imageWidth/this.numFrames,this.imageHeight/this.rows,this.x,this.y,this.width,this.height);
+         
+};
+Sprite.prototype.aniDir = function (rowIndex) {
+    this.rowIndex=rowIndex;
+}
+
 Sprite.prototype.update = function () {
 
-if (this.visible){
+    if (this.visible) {
 
-    this.context.save();
-    //this.context.translate(this.xPos, this.yPos);
-    this.context.drawImage(this.image,this.x,this.y);
-    this.context.restore();
-}
+        this.context.save();
+        //this.context.translate(this.xPos, this.yPos);
+        if(this.animation===1){
+            this.context.drawImage(this.image,this.frameIndex*this.imageWidth/this.numFrames,this.rowIndex*this.imageHeight/this.rows,this.imageWidth/this.numFrames,this.imageHeight/this.rows,this.x,this.y,this.width,this.height);
+        }else{
+        this.context.drawImage(this.image, this.x, this.y);
+    }
+        this.context.restore();
+    }
 
 };
 
@@ -140,14 +218,195 @@ Sprite.prototype.isVisible = function () {
     return this.visible;
 
 };
-Sprite.prototype.show = function(){
+Sprite.prototype.show = function () {
 
-	this.visible = true;
+    this.visible = true;
+
+};
+Sprite.prototype.hide = function () {
+
+    this.visible = false;
+
+};
+Sprite.prototype.collision = function (otherX, otherY,width,height) {
+   
+    if ((otherX >= this.x && otherX <= (this.x + this.width)) || ((otherX + width) >= this.x && (otherX + width) <= (this.x + this.width))) {
+
+        //topside and  bottomside
+        if ((otherY >= this.y && otherY <= (this.y + this.height)) || ((otherY + height) >= this.y && ( (otherY + height) <= (this.y + this.height)))) {
+            
+            //Don't move if shape is in this area
+            
+            return true;
+
+        }
+    }
+    return false;
+};
+
+function Projectile(world, x, y, radius, color, id) {
+
+    this.world = world;
+    this.canvas = world.canvas;
+    this.context = world.canvas.getContext("2d");
+    this.id = id;
+    this.radius = radius;
+    this.color=color;
+
+    this.direction = id;
+
+    this.x = x;
+    this.y = y;
+    this.xPos=x+(this.radius/2);
+    this.yPos=y+(this.radius/2);
+    
+    this.speed = 5;
+    this.direction = 4;
+    //this.context.drawImage(this.image,x,y);
+
+
+}
+Projectile.prototype.draw = function (x, y) {
+    this.x = x;
+    this.y = y;
+    
+    //reset direction
+    this.direction=4;
+    
+    this.context.beginPath();
+    this.context.fillStyle = this.color;
+    this.context.arc(x, y, this.radius, 0, 360);
+    this.context.closePath();
+    this.context.fill();
+};
+Projectile.prototype.move = function () {
+    if (this.direction === 4) {
+        //left
+        this.x =  this.x + this.speed;
+    }
+    else if (this.direction === 1) {
+        //up
+        this.y =  this.y - this.speed ;
+    }
+    else if (this.direction === 2) {
+        //down
+        this.y =  this.y + this.speed;
+    }
+    else if (this.direction === 3) {
+        //right
+        this.x =  this.x-this.speed;
+    }
+    //console.log(this.x+" "+this.y);
+    this.xPos=this.x+(this.radius);
+    this.yPos=this.y+(this.radius);
+
+};
+Projectile.prototype.getDir = function () {
+    return this.direction;
+
+};
+Projectile.prototype.changeDir = function (direction) {
+    this.direction = direction;
+
+};
+Projectile.prototype.collision = function (otherX, otherY,width,height) {
+   
+    if (this.xPos >= (otherX+(width/3)) && this.xPos < (otherX + width-(width/3))){
+
+//console.log(otherY + height-(height/3))+"  Y "+ otherY+(height/3));
+        //topside and  bottomside
+        if (this.yPos >= (otherY+(height/3)) && this.yPos < (otherY + height-(height/3))){
+            //Don't move if shape is in this area
+            //console.log("bounce");
+            return true;
+
+        }
+    }
+    return false;
+};
+Projectile.prototype.update = function () {
+    this.context.beginPath();
+    this.context.fillStyle = this.color;
+    this.context.arc(this.x, this.y, this.radius, 0, 360);
+    this.context.closePath();
+    this.context.fill();
+};
+
+
+//Timer class
+function Timer(startTime, endTime, interval){
+
+	this.current = startTime;
+	//this.endTime = endTime;
+	this.elapsed = 0;
+	//this.animTime = 0;
+	//this.interval = interval;
+	self = this;
+
+}
+
+Timer.prototype.stop = function(){
+
+	//Stop the timer
+	clearInterval(self.updateTime);
 
 };
 
-Sprite.prototype.hide = function(){
+Timer.prototype.getCurT = function(){
 
-	this.visible = false;
+	return self.current;
 
 };
+Timer.prototype.setCurT = function(time){
+
+	self.current = time;
+
+};
+
+Timer.prototype.getElapsed = function(){
+
+	return self.elapsed;
+
+};
+
+Timer.prototype.setElapsed = function(time){
+
+	self.elapsed = time;
+
+};
+//
+//Timer.prototype.startAnimCount = function(){
+//
+	//Set the timer to count up every "interval" milliseconds
+//	this.updateTime = setInterval(self.animCount, self.interval);
+//
+//}
+//Timer.prototype.getAnimTime = function(){
+//
+//	return self.animTime;
+//
+//}
+//
+//Timer.prototype.setAnimTime = function(time){
+//
+//	self.animTime = time;
+//
+//}
+//
+//Timer.prototype.countUp = function(){
+//
+//	self.elapsedTime++;
+//
+//}
+//
+//Timer.prototype.countDown = function(){
+//
+//	self.currentTime--;
+//
+//}
+//
+//Timer.prototype.animCount = function(){
+//
+//	self.animTime++;
+//
+//}
